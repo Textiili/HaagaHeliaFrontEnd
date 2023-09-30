d1589cce8176420d1528fe0c37c5b446c797c333
Textiili <erkki.tiili@gmail.com>
2023-09-30 20:03:28 +0300

    todolist with delete

--- src/App.jsx
+++ src/App.jsx
@@ -15,26 +15,31 @@ function App() {
     setTodos([...todos, todo]);
   }
 
+  const removeTodo = (index) => {
+    setTodos(todos.filter((todo, i) => i !== index));
+  }
+
   return (
     <div>
-      <h1>Simple todolist</h1>
-      <label for="addtodo">Add todo:</label>
+      <h1>Todolist with delete</h1>
+      <label htmlFor="addtodo">Add todo:</label>
       <form id="addtodo" onSubmit={addTodo}>
-        <label for="date">Date: </label>
+        <label htmlFor="date">Date: </label>
         <input id="date" type="date" name="date" value={todo.date} onChange={inputChanged}/>
-        <label for="description">Description: </label>
+        <label htmlFor="description">Description: </label>
         <input id="description" type="text" name="desc" value={todo.desc} onChange={inputChanged}/>
         <input type="submit" value="Add"/>
       </form>
       <br />
       <table>
        <tbody>
-       <tr><th>Date</th><th>Description</th></tr>
+       <tr><th>Date</th><th>Description</th><th></th></tr>
         {
           todos.map((todo, index) =>
           <tr key="index">
             <td>{todo.date}</td>
             <td>{todo.desc}</td>
+            <td><button onClick={() => removeTodo(index)}>Delete</button></td>
           </tr>
           )
         }
